076de655e7597cfbec14bffd0a5984c5360b8b1c
Brooke Hasty <brookehasty777@gmail.com>
2026-06-14 13:16:19 -0400

    Debit and Credit Tabs

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def load_transactions(file):
         df.columns = [col.strip() for col in df.columns] # remove white spaces
         df["Amount"] = df["Amount"].str.replace(",", "").astype(float) # remove commas/make float
         df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
-        st.write(df)
 
         return df
     except Exception as e:
@@ -28,7 +27,16 @@ def main():
         df = load_transactions(uploaded_file)
 
         if df is not None:
+            # seperate debit and credit col
             debits_df = df[df["Debit/Credit"] == "Debit"].copy()
             credits_df = df[df["Debit/Credit"] == "Credit"].copy()
+
+            tab1, tab2 = st.tabs(["Expenses (Debits)", "Payments (Credits)"])
+
+            with tab1:
+                st.write(debits_df)
+
+            with tab2:
+                st.write(credits_df)
 
 main()
